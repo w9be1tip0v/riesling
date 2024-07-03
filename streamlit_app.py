eeebec532e5398183ec4faddcb596daae7180f0b
Avada Kedavra <accounts@abcdef.gg>
2024-07-04 01:27:12 +0900

    Change auth system

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ st.set_page_config(
 # Read secrets
 API_KEY = st.secrets["API_KEY"]
 if API_KEY is None:
-    st.error("API_KEY is not set in .env file")
+    st.error("API_KEY is not set in secret.toml file")
     st.stop()
 
 
@@ -366,19 +366,15 @@ else:
 # Display the title of the app
 st.title(':hatched_chick: Polygon Data Viewer')
 
-# Set the app mode to 'Select' if it's not set
-if 'app_mode' not in st.session_state:
-    st.session_state.app_mode = 'Select'
-
-# Sidebar to select the market data to view
-st.session_state.app_mode = st.sidebar.selectbox(
-    'Choose the Market Data to View:',
-    ['Select', 'Company Detail', 'Historical Stock Data', 'Company Financials Data', 'Stock Splits Data', 'Dividends Data']
-)
+# Sidebar
+app_mode = st.sidebar.selectbox(
+        'Choose the Market Data to View:',
+        ['Select', 'Company Detail', 'Historical Stock Data', 'Company Financials Data', 'Stock Splits Data', 'Dividends Data']
+    )
 
 
 # Top-level header
-if st.session_state.app_mode == 'Select' and st.session_state['authenticated']:
+if app_mode == 'Select':
     st.header('Latest News')
     # Get news data and display it
     news_data = get_news()
@@ -415,7 +411,7 @@ if st.session_state.app_mode == 'Select' and st.session_state['authenticated']:
 
 
 # Historical Stock Data
-elif st.session_state.app_mode is 'Historical Stock Data' and st.session_state['authenticated'] is True:
+elif app_mode is 'Historical Stock Data':
     st.header("Historical Stock Data")
     ticker = st.text_input('Enter ticker symbol', 'AAPL')
     timespan = st.selectbox('Select timespan', options=['minute', 'hour', 'day', 'month', 'year'], index=2)  # Default to 'day'
@@ -434,7 +430,7 @@ elif st.session_state.app_mode is 'Historical Stock Data' and st.session_state['
 
 
 # Financials Data
-elif st.session_state.app_mode is 'Company Financials Data' and st.session_state['authenticated'] is True:
+elif app_mode is 'Company Financials Data':
     st.header("Company Financials Data")
     ticker = st.text_input('Enter ticker symbol', 'AAPL')
     limit = st.number_input('Enter the number of financial records to retrieve (min=1, max=100)', min_value=1, max_value=100, value=30) # Default to 30
@@ -450,7 +446,7 @@ elif st.session_state.app_mode is 'Company Financials Data' and st.session_state
 
 
 # Company Detail
-elif st.session_state.app_mode is 'Company Detail' and st.session_state['authenticated'] is True:
+elif app_mode is 'Company Detail':
     st.header("Company Detail")
     ticker = st.text_input('Enter ticker symbol', 'AAPL').upper()
     
@@ -524,7 +520,7 @@ elif st.session_state.app_mode is 'Company Detail' and st.session_state['authent
             st.error(str(e))
 
 # Stock Splits Data
-elif st.session_state.app_mode is 'Stock Splits Data' and st.session_state['authenticated'] is True:
+elif app_mode is 'Stock Splits Data':
     st.header("Stock Splits Data")
     ticker = st.text_input('Enter ticker symbol (optional)')
 
@@ -551,7 +547,7 @@ elif st.session_state.app_mode is 'Stock Splits Data' and st.session_state['auth
         display_data_with_default_sort(df_splits, 'Execution Date')
 
 # Dividends Data
-elif st.session_state.app_mode is 'Dividends Data' and st.session_state['authenticated'] is True:
+elif app_mode is 'Dividends Data':
     st.header("Dividends Data")
     ticker = st.text_input('Enter ticker symbol', 'AAPL').upper()
     limit = st.number_input('Limit', min_value=1, max_value=1000, value=50, step=1)
