1d0d40f2201da335fe9d2b80dfe1723eeb6b0cb8
Avada Kedavra <accounts@abcdef.gg>
2024-07-06 14:24:09 +0900

    Change auth system

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,8 @@ import logging
 from logging.handlers import TimedRotatingFileHandler
 from datetime import datetime
 import base64
-from json
+from jose import jwt
+from jose.exceptions import JWTError, ExpiredSignatureError, JWTClaimsError
 
 # Metadata
 st.set_page_config(
@@ -109,6 +110,20 @@ def get_jwks():
     else:
         raise Exception("Failed to fetch JWKS data")
 
+# Get public key from JWKS
+def get_public_key(token):
+    jwks = get_jwks()
+    unverified_header = jwt.get_unverified_header(token)
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:
+            return {
+                'kty': key['kty'],
+                'crv': key['crv'],
+                'x': key['x'],
+                'y': key['y'],
+            }
+    raise JWTError('Public key not found.')
+
 # Decode JWT token and get claims
 def decode_id_token(token):
     parts = token.split('.')
@@ -117,17 +132,15 @@ def decode_id_token(token):
 
     payload = parts[1]
     decoded_payload = base64.urlsafe_b64decode(payload + "==")
-    claims = json.loads(decoded_payload)
+    claims = jwt.decode(decoded_payload, verify=False)
     return claims
 
 # Authenticate request
 def authenticate_request():
-    st.write("Query parameters:", st.query_params)  # Debugging line
-    token = st.query_params.get("token", [None])[0]
-    st.write("Token from query parameters:", token)  # Debugging line
+    token = st.query_params.get("token")
     if not token:
         return None, "missing required Logto-ID-Token parameter"
-
+    
     try:
         claims = decode_id_token(token)
         return claims, None
@@ -336,10 +349,14 @@ def plot_candlestick_chart(df):
 ### Authentication ###
 if 'user_info' not in st.session_state:
     user_info, auth_error = authenticate_request()
-    st.write("User info:", user_info)  # Debugging line
-    st.write("Auth error:", auth_error)  # Debugging line
     if auth_error:
+        redirect_script = f"""
+        <script type="text/javascript">
+            window.location.href = "https://riesling.protected.app";
+        </script>
+        """
         st.error(f"Authentication error: {auth_error}")
+        st.markdown(redirect_script, unsafe_allow_html=True)
         st.stop()
     st.session_state['user_info'] = user_info
 
